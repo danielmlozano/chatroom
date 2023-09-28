98e6893407e63055779b21fe201ada16e3307b94
realdanielmlozano <daniel@danielmlozano.com>
2023-09-28 15:17:47 -0600

    Use typescript forVue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": "/src",
+			"@Components": "/src/Components",
+			"@Pages": "/src/Components/Pages",
+			"@Forms": "/src/Components/Forms",
+			"@Composables": "/src/Composables",
+		},
+	},
+	server: {
+		port: 3000,
+	},
 })
